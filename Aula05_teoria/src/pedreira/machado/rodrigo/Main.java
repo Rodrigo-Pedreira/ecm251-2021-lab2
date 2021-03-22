081e198305f6f58db9ade84ac8d3058c755bc86b
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2021-03-22 09:00:19 -0300

    Fim Aula05_teoria

---
+++ Aula05_teoria/src/pedreira/machado/rodrigo/Main.java
@@ -0,0 +1,8 @@
+package Aula05_teoria.src.pedreira.machado.rodrigo;
+
+public class Main {
+    public static void main(String[] args) {
+        String cpf = "629.982.247-25"; //
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
